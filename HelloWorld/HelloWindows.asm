a84268e3afe926168936a58a8911f408f2e358e6
Alex Sobiek <alex@alexsobiek.com>
2021-05-02 01:13:36 -0500

    More formatting fixes

--- HelloWorld/HelloWindows.asm
+++ HelloWorld/HelloWindows.asm
@@ -10,12 +10,12 @@ section	.text
 _main:                  ; Write our assembly for main
 	push	msg         ; Push our message to the stack
 	call	_printf     ; Call printf for our message
-	add	esp, 4      ; Clear the stack
+	add	esp, 4          ; Clear the stack
 	ret                 ; Return
 
 section	.data
 	msg:	db	"Hello Windows!", 0xA, 0    ; Defines "Hello Windows!" as bytes
 
-    ; Note on 0xA and 0
-    ; 0xA  hex for a new line
-    ; 0    defines the end of line
+; Note on 0xA and 0
+; 0xA  hex for a new line
+; 0    defines the end of line
