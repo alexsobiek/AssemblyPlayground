b22d21d8070731684f82f98d9e2eed70320cbea0
Alex Sobiek <alex@alexsobiek.com>
2021-05-02 01:09:55 -0500

    Cleaned up

--- HelloWorld/HelloWindows.asm
+++ HelloWorld/HelloWindows.asm
@@ -3,19 +3,19 @@
 ; Simple win32 executable that outputs "Hello Windows!" to the command line
 ; Written as part of the AssemblyPlayground repository on GitHub
 
-    global _main        ; Define main
-    extern _printf      ; Use printf()
+global  _main           ; Define Main
+extern  _printf         ; Use printf
 
-    segment .data
-    message: db "Hello Windows!", 0xA, 0    ; Defines "Hello World" as bytes
+section	.text
+_main:                  ; Write our assembly for main
+	push	msg         ; Push our message to the stack
+	call	_printf     ; Call printf for our message
+	add		esp, 4      ; Clear the stack
+	ret                 ; Return
+
+section	.data
+	msg:	db	"Hello Windows!", 0xA, 0    ; Defines "Hello Windows!" as bytes
 
     ; Note on 0xA and 0
     ; 0xA  hex for a new line
     ; 0    defines the end of line
-
-    section .text
-_main:              ; Write our assembly for main
-    push message    ; Push our message to the stack
-    call _printf    ; Call printf for our message
-    add esp, 4      ; Clear the stack
-    ret             ; Return
