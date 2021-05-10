50f31640017875484768708366b0d1de02c41259
Alex Sobiek <alex@alexsobiek.com>
2021-05-10 01:02:14 -0500

    Working Boot Loader

--- HelloBootLoader/bootloader.asm
+++ HelloBootLoader/bootloader.asm
@@ -1,27 +1,35 @@
 ; Hello Boot loader - Extremely simple boot loader written to print text
 ; Author: Alexander Sobiek, 2021 - Heavily inspired by Eugene Obrezkov
 ; Major credits to https://blog.ghaiklor.com/2017/10/21/how-to-implement-your-own-hello-world-boot-loader/
+; and http://kernelx.weebly.com/writing-a-bootsector.html
 
-org 0x7C00                                      ; Load our OS into this address
+org 0x7C00                                      ; Load our boot loader into this address
 bits 16                                         ; Define this program to be in 16 bit mode
 
-
 start:
-        cli                                     ; Disable Interupts
-        mov si, msg                             ; Move our msg into SI
-        mov ah, 0x0E                            ; Instruct the BIOS to print characters
-.loop   lodsb                                   ; Load SI into AL and increment SI for the next character
-        or  al, al                              ; Check if at end of string
-        jz  halt                                ; If so, jump to halt
-        int 0x10                                ; If not, call interupt to print the character
-        jmp loop                                ; Continue the loop
+    msg db "Hello, Boot Loader!", 0xA, 0; Define Hello, Boot Loader! as bytes and insert a new line
+    mov si, msg                         ; You might want your text to be displayed
+    call print                          ; Call print to print msg
+    jmp bootsector                      ; Jump to the bootsector
 
-halt:   hlt                                     ; Instruct the CPU to stop execution
-msg:    db "Hello, Boot Loader!", 0xA, 0        ; Define Hello, Boot Loader! as bytes and insert a new line
+print:                                  ; Printing Function
+    lodsb                               ; Load SI into AL and increment SI for the next character
+    or  al, al                          ; Check if at end of string
+    jz  done                            ; If so, return
+    mov ah, 0Eh                         ; Set the function number in AH
+    int 10H                             ; Call our helpful BIOS to do the rest, we are lazy
+    jmp print                           ; Go back to print the next character
 
-; Create our boot sector
-; Note: NASM gets stuck on this as it expects an instruction.
-; I am working on attempting to understand what it wants here
-; and what I can do to make it functional.
-times 510 – ($ – $$) db 0                       ; Create 510 empty bytes
-dw    0xaa55                                    ; Append bytes 55 and aa
+done:
+    ret                                 ; return
+
+bootsector:
+    cli                                 ; Disable interupts, prevents halting
+    hlt                                 ; halt
+    times 510 - ($-$$) db 0             ; Create 510 empty bytes
+    dw 0xAA55                           ; Append bytes 55 and AA (boot signature)
+
+; Anatomy of the Boot Sector (512 bytes, 510 empty and 0xAA55 as the last two)
+; 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+; 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+; 00 00 00 00 00 00 00 00 00 00 00 00 00 00 55 aa
